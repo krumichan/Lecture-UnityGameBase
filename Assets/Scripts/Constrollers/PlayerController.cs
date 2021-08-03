b3146c7d5d4abd418920b0fa8c518d78d4c8ae5c
KrumiChan <lovelybarry@naver.com>
2021-08-03 21:33:06 +0900

    Login/Game Scene 및 Scene Manager 추가.

--- Assets/Scripts/Constrollers/PlayerController.cs
+++ Assets/Scripts/Constrollers/PlayerController.cs
@@ -19,9 +19,6 @@ public class PlayerController : MonoBehaviour
 
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
-
-        // TEMP
-        Managers.UI.ShowSceneUI<UI_Inventory>();
     }
 
     public enum PlayerState
--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -8,12 +8,13 @@ public class Managers : MonoBehaviour
     static Managers Instance { get { Init(); return s_instance; } }
 
     InputManager _input = new InputManager();
-    public static InputManager Input { get { return Instance._input; } }
-
     ResourceManager _resource = new ResourceManager();
-    public static ResourceManager Resource { get { return Instance._resource; } }
-
+    SceneManagerEx _scene = new SceneManagerEx();
     UIManager _ui = new UIManager();
+
+    public static InputManager Input { get { return Instance._input; } }
+    public static ResourceManager Resource { get { return Instance._resource; } }
+    public static SceneManagerEx Scene { get { return Instance._scene; } }
     public static UIManager UI { get { return Instance._ui; } }
 
     void Start() 
